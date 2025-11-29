df0637d55a69b4aec453f0aac9521b4505e364db
varun pc <varunrevati6@gmail.com>
2025-11-29 19:53:58 +0530

    Common: firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
-import 'package:flutter/material.dart'; 
-import 'package:wdr/app/common/di_models.dart'; 
-import 'package:wdr/app/routes/app_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+import 'package:wdr/app/common/di_models.dart';
+import 'package:wdr/app/routes/app_pages.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
